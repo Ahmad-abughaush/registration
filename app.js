501bf9b1fb8f03323676af821f9e137b55211307
Ahmad-abughaush <ahmedabughoshh@gmail.com>
2023-04-11 11:26:15 +0300

    the code have fixed

--- app.js
+++ app.js
@@ -4,91 +4,76 @@ if (sessionStorage.arr != null) {
     arr = JSON.parse(sessionStorage.arr);
 }
 
+let Name;
+let Password;
+let Email;
+let number;
 
+let input = document.getElementById("input");
 
-let input = document.getElementById("input")
 input.addEventListener("submit", function (event) {
-    event.preventDefault()
+    event.preventDefault();
 
+    Name = event.target.fullname.value;
+    Password = event.target.password.value;
+    Email = event.target.email.value;
+    number = event.target.phonenumber.value;
 
-    let Name = event.target.fullname.value
-    let Phonenumber = event.target.phonenumber.value
-    let Email = event.target.email.value
-    let Password = event.target.password.value
-
-
-
-    if (validateName() && validatePhonenumber() && validateEmail && validatePassword()) {
-        return arr.push(Name);
-        sessionStorage.setItem("arr", JSON.stringify(arr))
+    if (validateName() && validatePassword() && validateEmail() && validateNumber()) {
+        arr.push(Name);
+        sessionStorage.setItem('arr', JSON.stringify(arr));
+        input.reset();
     }
-    else { return false }
-
 
 });
 
+function validateName() {
+    Name = Name.toLowerCase();
+    if (Name == "") {
+        alert("Name must be filled out");
+        return false;
 
-validateName()
-{
-    let username = Name.tolowercase()
-    if (username == "" || username.includes(" ")) {
-        alert("the username must be filled as well ");
-        return false
-    }
+    } else if (Name.includes(" ")) {
+        alert("Name must be filled with no spaces");
+        return false;
+    } else if (arr.includes(Name)) {
 
-
-    else if (arr.includes(username)) {
-        alert(" the name is exists");
-        return false
-    }
-
-    else { return true }
-
-}
-
-
-
-validatePassword(){
-
-    let password = Password
-    let spchar = "!#$%^&*()+=-[]\\\';,/{}|\":<>?";
-    let passwordregx = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,}$/;
-    if (!passwordregx.test(password)) {
-        alert
-            ("Password must contain at least 8 characters, 1 number, 1 uppercase letter, and 1 special character")
-    }
-    return false
-}
-
-function validateEmail() {
-    let iChars = "!#$%^&*()+=-[]\\\';,/{}|\":<>?";
-    let Email = userEmail.toLowerCase();
-    if (!Email.includes("@") || !Email.includes(".com")) {
-        alert("Please enter a valid Email EX.. majdi@gamil.com");
+        alert("Username already exists. welcome " + Name);
         return false;
     }
-    for (let i = 0; i < Email.length; i++) {
-        if (iChars.indexOf(Email.charAt(i)) != -1) {
-            alert("The Email has special characters. \nThese are not allowed.\n");
-            return false;
-        }
+
+    return true;
+
+}
+
+
+function validatePassword() {
+    let password = Password;
+    const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+    if (!passwordRegex.test(password)) {
+        alert("Password must contain at least 8 characters, 1 number, 1 uppercase letter, and 1 special character");
+        return false;
     }
     return true;
 }
-function validateNumber() {
-    let iChars = "!#$%^&*()+=-[]\\\';,/{}|\":<>?@.";
-    let Number = Phonenumber;
 
-    if (!(Number.includes("07")) || !(Number.length == 10)) {
-        alert("Please enter a valid number ex. 0799855850");
+
+function validateEmail() {
+    if (!/\S+@\S+\.\S+/.test(Email)) // search for at least non-whitespace characters
+    {
+        alert("E-mail must be in a valid format such as example@gmail.com");
         return false;
     }
+    return true;
 
-    for (let i = 0; i < Number.length; i++) {
-        if (iChars.indexOf(Number.charAt(i)) != -1) {
-            alert("The Number has special characters. \nThese are not allowed.\n");
-            return false;
-        }
+}
+
+
+// Validate phone number to be " 10 digits starts with 07 "
+function validateNumber() {
+    if (!/^07[0-9]{8}$/.test(number)) {
+        alert("Phone number must be 10 digits starting with 07");
+        return false;
     }
     return true;
 }
